67259da5db5360625048c7d536d8eb6ead7f02d8
OlebogengRasebitse <patiencerasebitse@gmail.com>
2023-03-30 08:14:38 +0200

    Deleted configuration.js

--- IWA3/Challenge1/configuration.js
+++
@@ -1,7 +0,0 @@
-const company = [hello;]
-const year = 2022 ;
-
-console.log ("company");
-
-const myArr = ["Orange", "Banana", "Mango", "Kiwi"];
-console.log(myArr);
--- IWA3/Challenge1/scripts.js
+++ IWA3/Challenge1/scripts.js
@@ -3,5 +3,5 @@
 
 
 
-/* console.log(year) */
+
 
