a985df9334ba2cc4aa158d75716d52de2d4d5de6
OlebogengRasebitse <patiencerasebitse@gmail.com>
2023-03-30 08:14:17 +0200

    Added configure.js untracked file

---
+++ IWA3/Challenge1/configure.js
@@ -0,0 +1,9 @@
+/** 
+export const company = 'ACME Inc.';
+const year = 2022;
+console.log(year) 
+
+export const company = '© ' + company + " + year + "
+document.querySelector('#footer').innerText = company*/
+const value = 123
+console.log(value)
