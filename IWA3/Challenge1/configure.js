43cd863948805c93b4c407977ec66b1ab2d00cbb
OlebogengRasebitse <patiencerasebitse@gmail.com>
2023-03-30 08:30:26 +0200

    Moved const message to script.js

--- IWA3/Challenge1/configure.js
+++ IWA3/Challenge1/configure.js
@@ -1,9 +1,4 @@
-/** 
-export const company = 'ACME Inc.';
-const year = 2022;
+export const company = "ACME Inc.";
+export const year = 2022;
 console.log(year) 
 
-export const company = '© ' + company + " + year + "
-document.querySelector('#footer').innerText = company*/
-const value = 123
-console.log(value)
--- IWA3/Challenge1/scripts.js
+++ IWA3/Challenge1/scripts.js
@@ -1,6 +1,9 @@
 // scripts.js
+import { company } from "./configure.js"
+import { year } from "./configure.js"
 
-
+const message = '© ' + company +  year 
+document.querySelector('#footer').innerText = message
 
 
 
