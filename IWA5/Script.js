64787021cd820327ccc873c7627a7978f2525796
OlebogengRasebitse <patiencerasebitse@gmail.com>
2023-04-02 01:16:19 +0200

    Edited code for bugs

--- IWA5/Script.js
+++ IWA5/Script.js
@@ -1,34 +1,67 @@
-const FREE_WARNING = 'Free shipping only applies to single customer orders';
-const BANNED_WARNIN = 'Unfortunately we do not ship to your country of residence';
+const FREE_WARNING =('Free shipping only applies to single customer orders');
+const BANNED_WARNING = ('Unfortunately we do not ship to your country of residence');
+
+const customers = ('1');
+const Location = ('RSA');
 
 
-if (location = RSA) { shipping === 400 && currency === 'R' }
+let shipping; 
+if (Location === 'RSA' ) {
+   shipping = 400;
+}
+else if (Location === 'NAM' ) {
+   shipping = 600;
+}
+else if (location === 'NK') {
+   shipping = null;
+console.log(BANNED_WARNING); 
 
-if location = NAM
-shipping = 600 
-else shipping = 800
-
-shoes = 300 * 1
-toys - 100 * 5
-shirts = 150 * 'NONE_SELECTED'
-batteries 35 * 2
-pens = 5 * 'NONE_SELECTED' 
-
-shipping = null
-currency = $
-
-if (shoes + batteries + pens + shirts > 1000 &&  ) {
-	if (location = NAM && customers < 2) {
-			if (location = RSA)
-		    shipping = 0 || calcShipping
-		}
-	}
 }
 
-if (shipping = 0) && (customers !=== 1) { console.log(WARNING) }
+else {shipping = 800;}
 
-location = 'NK' ? console.log(WARNING) : console.log('price', currency, shoes + batteries + pens + shirts + shipping)
 
-customers = '1'
-const location = 'RSA'
-currency = null
+
+let currency; 
+if (Location === 'RSA' ) {
+   currency = R;
+}
+else if (Location === 'NAM' ) {
+   currency = $;
+}
+else if (Location === 'NK') {
+   currency = $;
+}
+
+
+else {shipping = 800;}
+
+
+const shoes = 300 * 1;
+const toys = 100 * 5;
+const shirts = 150 * 0;
+const batteries = 35 * 2;
+const pens = 5 *0 ;
+
+
+const subtotal = (shoes + toys + shirts + batteries + pens);
+
+if (subtotal >= 1000 && customers === 1) {
+   if (location === 'RSA' || location === 'NAM'){
+      shipping = 0;
+   }
+   else {
+      console.log(FREE_WARNING);
+   }
+}
+
+if (shipping === 0 && customers !== 1) {
+   console.log(FREE_WARNING);
+}
+
+if (shipping === null) {
+   console.log('Price:', currency, subtotal + shipping)
+   
+}
+console.log('Price:', currency, subtotal + shipping)
+
