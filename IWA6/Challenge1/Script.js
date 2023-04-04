ab2c5431bd056e654a087092ffca1ecf56a4bf51
OlebogengRasebitse <patiencerasebitse@gmail.com>
2023-04-04 11:06:18 +0200

    used regular expressions in code.

--- IWA6/Challenge1/Script.js
+++ IWA6/Challenge1/Script.js
@@ -5,12 +5,12 @@ const secondaryPhone = '0219131568';
 
 //Only change below this line
 
-const primaryValid = typeof number === primaryPhone 
-const secondaryValid = typeof number === secondaryPhone
+const primaryValid = typeof primaryPhone === 'string' && /^[0-9]+$/.test(primaryPhone)
+
+const secondaryValid = typeof secondaryPhone === 
+'string' && /^[0-9]+$/.test(secondaryPhone)
 
 console.log('Primary phone is valid numerical string:', primaryValid)
 
 
-
-
-console.log('Secondary phone is valid numerical string:', secondaryValid )
+console.log('Secondary phone is valid numerical string:', secondaryValid)
