cdf461938ab0e46dff754f4b64617cc876aedab0
OlebogengRasebitse <patiencerasebitse@gmail.com>
2023-04-04 13:46:43 +0200

    Fixed balance spelling error & added if statement.

--- IWA6/Challenge2/scripts.js
+++ IWA6/Challenge2/scripts.js
@@ -8,10 +8,19 @@ const minuteOfDay = 00;
 
 // Only change below this line
 
-if hourOfDay && (minuteOfDay !== null) && (hourOfDay == '00') && (minuteOfDay == '00') {
-	const taxAsDecimal = tax / '100'
-  const startingAfterTax = salary * '1' - taxAsDecimal
-	const balace = starting - transport - food - rent
+if (hourOfDay && minuteOfDay === 00){
+    console.log(balance)
 }
-	
+else {
+    console.log(undefined)
+}
+
+
+
+if hourOfDay && (minuteOfDay !== null) && (hourOfDay == '00') && (minuteOfDay == '00') {
+    const taxAsDecimal = tax / '100'
+  const startingAfterTax = salary * '1' - taxAsDecimal
+    const balance = starting - transport - food - rent
+}
+    
 console.log(balance.toFixed(3))
