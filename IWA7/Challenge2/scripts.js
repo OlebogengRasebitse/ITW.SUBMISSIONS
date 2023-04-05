29c9840893dd15745eb4e25225551f4fa4155335
OlebogengRasebitse <patiencerasebitse@gmail.com>
2023-04-05 14:23:29 +0200

    Added HTML boilerplate and challenge heading.

--- IWA7/Challenge2/scripts.js
+++ IWA7/Challenge2/scripts.js
@@ -1,9 +1,9 @@
 const nickname= "Timmy";
 const firstname = "Timothy";
 
-if (!nickname) {
-    console.log(`Good Morning, ${nickname}!`) 
+if (nickname) {
+    console.log(`Good Morning, ${nickname}!`); 
 }
-else if (nickname) {
-    console.log(`Good Morning, ${firstname}!`)
+else {
+    console.log(`Good Morning, ${firstname}!`);
 }
