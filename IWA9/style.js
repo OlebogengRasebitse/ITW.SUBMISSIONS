ead87b6f339280b7e3aac974f136c476c9e5dd0c
OlebogengRasebitse <patiencerasebitse@gmail.com>
2023-04-07 23:42:50 +0200

    Edited code for bugs.

--- IWA9/style.js
+++ IWA9/style.js
@@ -6,8 +6,8 @@ const size = 'large'
 // Only change the syntax below (not the values or key names)
 
 const expenses = {
-    food: 51.7501,
-    transport:  10.2,
+    food: 51.7501 ,
+    transport: 10.2,
 }
   
 const tax = {
@@ -20,18 +20,20 @@ const tax = {
 
 const rent = {
     none: 0,
-    'small-room': 200,
-    'large-room': 300,
-    'small-apartment': 400,
-    'large-apartment': 800,
-    'small-house': 1200,
-    'large-house': 2400,
+    smallRoom : 200,
+    largeRoom : 300,
+    smallApartment: 400,
+    largeApartment : 800,
+    smallHouse : 1200,
+    largeHouse : 2400,
 }
 
 // You can change below however you want
 
-const taxAsDecimal = tax.913 / 100
-const startingAfterTax = salary * 1 - taxAsDecimal
-const type = lodging + size
-const balance = expenses(transport) - expenses(food) - expenses(rent.type) 
+ const taxAsDecimal =  parseFloat(tax[913])/100 
+ const startingAfterTax = salary - (taxAsDecimal * salary)
+ const type = lodging + size
+ const balance = (startingAfterTax - expenses.transport - expenses.food - rent.largeApartment).toFixed(2) 
+
 console.log(balance)
+
