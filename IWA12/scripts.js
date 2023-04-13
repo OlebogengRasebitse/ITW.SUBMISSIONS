2d2b44b54fe5c72212561ac3dca585c4e877437b
OlebogengRasebitse <patiencerasebitse@gmail.com>
2023-04-13 22:54:51 +0200

    fixed js variables syntex errors.

---
+++ IWA12/scripts.js
@@ -0,0 +1,65 @@
+// scripts.js
+
+const STATUS_MAP = {
+    shelf: {
+        color: 'green',
+        canReserve: true,
+        canCheckout: true,
+        canCheckIn: false,
+    },
+    reserved: {
+        color: 'blue',
+        canReserve: false,
+        canCheckout: true,
+        canCheckIn: false,
+    },
+    overdue: {
+        color: 'red',
+        canReserve: false,
+        canCheckout: false,
+        canCheckIn: true,
+    },
+    checkedOut: {
+        color: 'orange',
+        canReserve: false,
+        canCheckout: false,
+        canCheckIn: true,
+    }
+}
+
+// Edit below line 
+
+const status1 = document.querySelector ('#book1 .reserve')
+
+console.log(status1)
+const reserve = selector(reserve)
+const checkout = selector(checkout)
+const checkin = selector(checkin)
+
+const status2 = selector(status)
+reserve = selector(reserve)
+checkout = selector(checkout)
+checkin = selector(checkin)
+
+status = selector(status)
+reserve = selector(reserve)
+checkout = selector(checkout)
+checkin = selector(checkin)
+
+// checkin.0.color = none
+// status1.0.style.color = STATUS_MAP.status.color
+// reserve.0 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
+// checkout. 0 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
+// checkin.0 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
+
+// checkin.1.color = none
+// status.1.style.color = STATUS_MAP.status.color
+// reserve.1 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
+// checkout.1 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
+// checkin.1 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
+
+// checkin.2.color = none
+// status.2.style.color = STATUS_MAP.status.color
+// reserve.2 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
+// checkout.2 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
+// checkin.2 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
