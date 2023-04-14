56d68a7ce388f7fce14af75aac03f518d409b655
OlebogengRasebitse <patiencerasebitse@gmail.com>
2023-04-14 11:31:04 +0200

    Added colours and dissable buttons

--- IWA12/scripts.js
+++ IWA12/scripts.js
@@ -29,44 +29,44 @@ const STATUS_MAP = {
 
 // Edit below line 
 
+//Buttons 1
 const status1 = document.querySelector ('#book1 .status')
-
-
  const reserve1 = document.querySelector('#book1 .reserve')
-
-
  const checkout1 = document.querySelector('#book1 .checkout')
-
  const checkin1 = document.querySelector('#book1 .checkin')
 
+
+//Buttons 2
  const status2 = document.querySelector('#book2 .status')
-
  const reserve2 = document.querySelector('#book2 .reserve')
-
 const checkout2 = document.querySelector('#book2 .checkout')
  const checkin2 = document.querySelector('#book2 .checkin')
 
+ //Buttons 3
 const status3 = document.querySelector('#book3 .status')
 const reserve3 = document.querySelector('#book3 .reserve')
 const checkout3 = document.querySelector('#book3 .checkout')
 const checkin3 = document.querySelector('#book3 .checkin')
 
-console.log(checkin3.innerText)
+//Overdue
+ checkin1.style.color = '';
+ status1.style.color = 'red';
+ document.querySelector("#book1 .reserve").disabled = true
+ document.querySelector("#book1 .checkout").disabled = true
+ document.querySelector("#book1 .checkin").disabled = false
 
-checkin1.color = none
-// status1.0.style.color = STATUS_MAP.status.color
-// reserve.0 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
-// checkout. 0 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
-// checkin.0 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
+//Reserved
+ checkin2.style.color = '';
+ status2.style.color = 'blue'
+ document.querySelector("#book2 .reserve").disabled = true;
+ document.querySelector("#book2 .checkout").disabled =  false ;
+ document.querySelector("#book2 .checkin").disabled = true;
 
-// checkin.1.color = none
-// status.1.style.color = STATUS_MAP.status.color
-// reserve.1 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
-// checkout.1 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
-// checkin.1 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
+//Shelf
+ checkin3.style.color = '';
+ status3.style.color = 'green';
+ document.querySelector("#book3 .reserve").disabled = false;
+ document.querySelector("#book3 .checkout").disabled = false;
+ document.querySelector("#book3 .checkin").disabled = true;
 
-// checkin.2.color = none
-// status.2.style.color = STATUS_MAP.status.color
-// reserve.2 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
-// checkout.2 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
-// checkin.2 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
+  
