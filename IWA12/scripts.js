239162ee640164e1b9f44976d1f26e1a8484d1dc
OlebogengRasebitse <patiencerasebitse@gmail.com>
2023-04-14 14:27:00 +0200

    Used tenary to enable & disable buttons.

--- IWA12/scripts.js
+++ IWA12/scripts.js
@@ -29,44 +29,22 @@ const STATUS_MAP = {
 
 // Edit below line 
 
-//Buttons 1
-const status1 = document.querySelector ('#book1 .status')
- const reserve1 = document.querySelector('#book1 .reserve')
- const checkout1 = document.querySelector('#book1 .checkout')
- const checkin1 = document.querySelector('#book1 .checkin')
+//Variables
+const statusE = document.querySelectorAll('.status');
+const reserveButton = document.querySelectorAll('.reserve');
+const checkoutButton = document.querySelectorAll('.checkout');
+const checkinButton = document.querySelectorAll('.checkin');
 
+//Update each book's elements based on its status
+statusE.forEach((statusE, index) => {
+  const status = statusE.textContent;
+  statusE.style.color = STATUS_MAP[status].color;
+  reserveButton[index].disabled = !STATUS_MAP[status].canReserve;
+  checkoutButton[index].disabled = !STATUS_MAP[status].canCheckout;
+  checkinButton[index].disabled = !STATUS_MAP[status].canCheckIn;
 
-//Buttons 2
- const status2 = document.querySelector('#book2 .status')
- const reserve2 = document.querySelector('#book2 .reserve')
-const checkout2 = document.querySelector('#book2 .checkout')
- const checkin2 = document.querySelector('#book2 .checkin')
+  reserveButton[index].style.filter = 'grayscale(100%)';
+  checkoutButton[index].style.filter = 'grayscale(100%)';
+  checkinButton[index].style.filter = 'grayscale(100%)';
+});
 
- //Buttons 3
-const status3 = document.querySelector('#book3 .status')
-const reserve3 = document.querySelector('#book3 .reserve')
-const checkout3 = document.querySelector('#book3 .checkout')
-const checkin3 = document.querySelector('#book3 .checkin')
-
-//Overdue
- checkin1.style.color = '';
- status1.style.color = 'red';
- document.querySelector("#book1 .reserve").disabled = true
- document.querySelector("#book1 .checkout").disabled = true
- document.querySelector("#book1 .checkin").disabled = false
-
-//Reserved
- checkin2.style.color = '';
- status2.style.color = 'blue'
- document.querySelector("#book2 .reserve").disabled = true;
- document.querySelector("#book2 .checkout").disabled =  false ;
- document.querySelector("#book2 .checkin").disabled = true;
-
-//Shelf
- checkin3.style.color = '';
- status3.style.color = 'green';
- document.querySelector("#book3 .reserve").disabled = false;
- document.querySelector("#book3 .checkout").disabled = false;
- document.querySelector("#book3 .checkin").disabled = true;
-
-  
