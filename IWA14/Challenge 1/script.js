385ef6b9f21d80d43e4c8f50e99e125a462ba865
OlebogengRasebitse <patiencerasebitse@gmail.com>
2023-04-18 09:29:47 +0200

    Moved files to Challenge 1 folder.

---
+++ IWA14/Challenge 1/script.js
@@ -0,0 +1,18 @@
+ firstName = 'John';
+age = 35;
+ hobby = 'Coding';
+
+//The expected output is:
+
+// Hello, John (35). I love coding!
+// Hello, John (35). I love coding!
+
+
+ function myHobby(){
+  console(`Hello, ${firstName} ${age}. I love ${hobby}!`)
+ }
+
+myHobby()
+myHobby()
+
+
