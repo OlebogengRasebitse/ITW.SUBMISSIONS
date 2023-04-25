4f9c96262be8c14fb91dd31e30f71ad8bc976010
OlebogengRasebitse <patiencerasebitse@gmail.com>
2023-04-25 09:54:45 +0200

    new css, html & js files.

--- IWA16/script.js
+++ IWA16/script.js
@@ -66,43 +66,101 @@ const data = {
 // Only edit below this comment
 
 
-const formatDate = (dateString) => {
-	const date = new Date(dateString); //creates a date object from a date string
-	const day = date.getDate();
-	const month = MONTHS[date.getMonth()];
-	const year = date.getFullYear();
-	return `${day} ${month} ${year}`;
-};
-
-
-const formatTime = (minutes) => {
-	const hours = Math.floor(minutes / 60);
-	const mins = minutes % 60;
-	return `${hours.toString().padStart(2, '0')}:${mins.toString().padStart(2, '0')}`;
-};
-
-const displayAthleteData = (athlete) => {
-	const { id, firstName, surname, races } = athlete;
+const createHtml = (athlete) => {
+	const { firstName, surname, id, races } = athlete;
+	const [latestRace] = races.slice(-1);
+	const [time1, time2, time3, time4] = latestRace.time;
+	const latestRaceDate = new Date(latestRace.date);
+	const formattedDate = `${latestRaceDate.getDate()} ${MONTHS[latestRaceDate.getMonth()]} ${latestRaceDate.getFullYear()}`;
+	const totalRaceCount = races.length;
+	const totalTime = `${String(Math.floor((time1 + time2 + time3 + time4) / 60)).padStart(2, '0')}:${String((time1 + time2 + time3 + time4) % 60).padStart(2, '0')}`;
+  
 	const section = document.querySelector(`[data-athlete="${id}"]`);
+		
+	const title = document.createElement('h2');
+	title.textContent = `Athlete: ${id}`;
+  
+	const list = document.createElement('dl');
+  
+	const fullNameTerm = document.createElement('dt');
+	fullNameTerm.textContent = 'Full Name';
+  
+	const fullNameDesc = document.createElement('dd');
+	fullNameDesc.textContent = `${firstName} ${surname}`;
+  
+	const raceCountTerm = document.createElement('dt');
+	raceCountTerm.textContent = 'Total Races';
+  
+	const raceCountDesc = document.createElement('dd');
+	raceCountDesc.textContent = totalRaceCount;
+  
+	const latestDateTerm = document.createElement('dt');
+	latestDateTerm.textContent = 'Event Date (Latest)';
+  
+	const latestDateDesc = document.createElement('dd');
+	latestDateDesc.textContent = formattedDate;
+  
+	const latestTimeTerm = document.createElement('dt');
+  latestTimeTerm.textContent = 'Latest Race Time';
+  
+  const latestTimeDesc = document.createElement('dd');
+  latestTimeDesc.textContent = totalTime;
 
-	const latestRace = races[races.length - 1];
-	const totalRaces = races.length;
+	list.appendChild(fullNameTerm);
+	list.appendChild(fullNameDesc);
+	list.appendChild(raceCountTerm);
+	list.appendChild(raceCountDesc);
+	list.appendChild(latestDateTerm);
+	list.appendChild(latestDateDesc);
+	list.appendChild(latestTimeTerm);
+	list.appendChild(latestTimeDesc);
+  
+	section.appendChild(title);
+	section.appendChild(list);
+  }
 
-	const h2 = document.createElement('h2');
-	h2.textContent = `Athlete: ${firstName} ${surname}`;
 
-	const dl = document.createElement('dl');
-	dl.innerHTML = `
-	  <dt>Full Name</dt>
-	  <dd>${firstName} ${surname}</dd>
-	  <dt>Total Races</dt>
-	  <dd>${totalRaces}</dd>
-	  <dt>Event Date (Latest)</dt>
-	  <dd>${formatDate(latestRace.date)}</dd>
-	  <dt>Total Time (Latest)</dt>
-	  <dd>${formatTime(latestRace.time.reduce((a, b) => a + b))}</dd>
-	`;
 
-	section.appendChild(h2);
-	section.appendChild(dl);
-}
+
+
+
+// const formatDate = (dateString) => {
+// 	const date = new Date(dateString); //creates a date object from a date string
+// 	const day = date.getDate();
+// 	const month = MONTHS[date.getMonth()];
+// 	const year = date.getFullYear();
+// 	return `${day} ${month} ${year}`;
+// };
+
+
+// const formatTime = (minutes) => {
+// 	const hours = Math.floor(minutes / 60);
+// 	const mins = minutes % 60;
+// 	return `${hours.toString().padStart(2, '0')}:${mins.toString().padStart(2, '0')}`;
+// };
+
+// const displayAthleteData = (athlete) => {
+// 	const { id, firstName, surname, races } = athlete;
+// 	const section = document.querySelector(`[data-athlete="${id}"]`);
+
+// 	const latestRace = races[races.length - 1];
+// 	const totalRaces = races.length;
+
+// 	const h2 = document.createElement('h2');
+// 	h2.textContent = `Athlete: ${firstName} ${surname}`;
+
+// 	const dl = document.createElement('dl');
+// 	dl.innerHTML = `
+// 	  <dt>Full Name</dt>
+// 	  <dd>${firstName} ${surname}</dd>
+// 	  <dt>Total Races</dt>
+// 	  <dd>${totalRaces}</dd>
+// 	  <dt>Event Date (Latest)</dt>
+// 	  <dd>${formatDate(latestRace.date)}</dd>
+// 	  <dt>Total Time (Latest)</dt>
+// 	  <dd>${formatTime(latestRace.time.reduce((a, b) => a + b))}</dd>
+// 	`;
+
+// 	section.appendChild(h2);
+// 	section.appendChild(dl);
+// }
